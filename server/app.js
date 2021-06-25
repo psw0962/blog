95487fb1140b48d48e3b63121f08f30d8195e0af
psw0962 <psw0962@naver.com>
2021-06-25 23:46:39 +0900

    api 테스트 : post api 테스트

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 import express from 'express'
-import mongoose from 'mongoose'
+import helmet from 'helmet';
+import hpp from 'hpp';
+import mongoose from 'mongoose';
 import morgan from 'morgan';
 import config from './config'
+import cors from 'cors';
+
+// Routes
+import postsRoutes from './routes/api/post';
 
 const app = express();
 const {MONGO_URI} = config
@@ -18,11 +24,13 @@ app.use(express.json());
 mongoose.connect(MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 })
 .then(() => console.log('MongoDB connecting Success!!'))
 .catch((e) => console.log(e))
 
 // Use routes
 app.get('/')
+app.use('/api/post', postsRoutes)
 
 export default app;
