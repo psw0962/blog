f1fafbee3b1f8ad6b2160dc117ca55bde88fe218
psw0962 <psw0962@naver.com>
2021-06-29 23:51:27 +0900

    router 셋팅, redux 셋팅

---
+++ client/src/routes/Router.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Myrouter = () => (
+    <Fragment>
+        <Header />
+        <h1>Hello Body</h1>
+        <Footer />
+    </Fragment>
+);
+
+export default Myrouter
