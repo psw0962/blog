19433f05f89676d5f0f41891fd5d3ad060dd2766
psw0962 <psw0962@naver.com>
2021-07-04 17:19:34 +0900

    cors설정 및 패키지 설정

--- server/app.js
+++ server/app.js
@@ -1,40 +1,43 @@
-import express from 'express'
-import helmet from 'helmet';
-import hpp from 'hpp';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import config from './config'
-import cors from 'cors';
+import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import config from "./config";
+import cors from "cors";
 
 // Routes
-import postRoutes from './routes/api/post';
-import userRoutes from './routes/api/user';
-import authRoutes from './routes/api/auth';
+import postRoutes from "./routes/api/post";
+import userRoutes from "./routes/api/user";
+import authRoutes from "./routes/api/auth";
 
 const app = express();
-const {MONGO_URI} = config
+const { MONGO_URI } = config;
 
 app.use(hpp());
 app.use(helmet());
-app.use(cors({
-    origin: true,
+app.use(
+  cors({
+    origin: "http://localhost:3000",
     credentials: true,
-}));
-app.use(morgan('dev'));
+  })
+);
+app.use(morgan("dev"));
 app.use(express.json());
 
-mongoose.connect(MONGO_URI,{
+mongoose
+  .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-})
-.then(() => console.log('MongoDB connecting Success!!'))
-.catch((e) => console.log(e))
+  })
+  .then(() => console.log("MongoDB connecting Success!!"))
+  .catch((e) => console.log(e));
 
 // Use routes
-app.get('/')
-app.use('/api/post', postRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
+app.get("/");
+app.use("/api/post", postRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
