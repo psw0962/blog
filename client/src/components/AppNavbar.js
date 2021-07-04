9e9f5ffc439b7cb17b838144381002855ef51012
psw0962 <psw0962@naver.com>
2021-07-04 17:18:44 +0900

    기능추가 : 로그인 인증에 따른 화면변화

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,30 +1,47 @@
-import React, { Fragment } from 'react';
-import {Navbar, Container, NavbarToggler, Collapse, Nav} from 'reactstrap';
-import {Link} from 'react-router-dom';
-import LoginModal from '../components/auth/LoginModal';
+import React, { Fragment, useCallback, useEffect, useState } from "react";
+import { Navbar, Container, NavbarToggler, Collapse, Nav } from "reactstrap";
+import { Link } from "react-router-dom";
+import LoginModal from "../components/auth/LoginModal";
+import { useSelector, useDispatch } from "react-redux";
+import { LOGOUT_REQUEST } from "../redux/types";
 
 const AppNavbar = () => {
-    return (
-        <Fragment>
-        <Navbar color='dark' dark expand='lg' className='sticky-top'>
-            <Container>
-                <Link to = '/' className='text-white text-decoration-none'>
-                    Nick's Blog
-                </Link>
-                <NavbarToggler />
-                <Collapse isopen='true' navbar>
-                    <Nav className='ml-auto d-flex justify-content-around' navbar>
-                        {false ? (
-                            <LoginModal />
-                        ) : (
-                        <LoginModal />
-                        )}
-                    </Nav>
-                </Collapse>
-            </Container>
-        </Navbar>
+  const [isOpen, setIsOpen] = useState(false);
+  const { isAuthenticated, user, userRole } = useSelector((state) => state.auth);
+  console.log(userRole, "UserRole");
+
+  const dispatch = useDispatch();
+  const onLogout = useCallback(() => {
+    dispatch({
+      type: LOGOUT_REQUEST,
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [user]);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Fragment>
+      <Navbar color="dark" dark expand="lg" className="sticky-top">
+        <Container>
+          <Link to="/" className="text-white text-decoration-none">
+            Nick's Blog
+          </Link>
+          <NavbarToggler onClick={handleToggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto d-flex justify-content-around" navbar>
+              {isAuthenticated ? <h1 className="text-white">authLink</h1> : <LoginModal />}
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
     </Fragment>
-    )
-}
+  );
+};
 
 export default AppNavbar;
--- client/src/redux/sagas/authSaga.js
+++ client/src/redux/sagas/authSaga.js
@@ -1,40 +1,57 @@
-import axios from 'axios';
-import { all, call, put, takeEvery, fork } from 'redux-saga/effects';
-import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from '../types';
+import axios from "axios";
+import { all, call, put, takeEvery, fork } from "redux-saga/effects";
+import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT_FAILURE, LOGOUT_REQUEST, LOGOUT_SUCCESS } from "../types";
 
 // Login
 const loginUserAPI = (loginData) => {
-    console.log(loginData, 'loginData')
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-    return axios.post('api/auth', loginData, config)
-}
+  console.log(loginData, "loginData");
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  return axios.post("api/auth", loginData, config);
+};
 
 function* loginUser(action) {
-    try {
-        const result = yield call(loginUserAPI, action.payload)
-        console.log(result)
-        yield put({
-            type: LOGIN_SUCCESS,
-            payload: result.data,
-        })
-    }catch(e) {
-        yield put({
-            type: LOGIN_FAILURE,
-            payload: e.response,
-        })
-    }
+  try {
+    const result = yield call(loginUserAPI, action.payload);
+    console.log(result);
+    yield put({
+      type: LOGIN_SUCCESS,
+      payload: result.data,
+    });
+  } catch (e) {
+    yield put({
+      type: LOGIN_FAILURE,
+      payload: e.response,
+    });
+  }
 }
 
 function* watchLoginUser() {
-    yield takeEvery(LOGIN_REQUEST, loginUser)
+  yield takeEvery(LOGIN_REQUEST, loginUser);
 }
 
+// Logout
+function* logout(action) {
+  try {
+    yield put({
+      type: LOGOUT_SUCCESS,
+    });
+  } catch (e) {
+    yield put({
+      type: LOGOUT_FAILURE,
+    });
+    console.log(e);
+  }
+}
+
+function* watchlogout() {
+  yield takeEvery(LOGOUT_REQUEST, logout);
+}
+
+// etc
 export default function* authSaga() {
-    yield all([
-        fork(watchLoginUser)
-    ])
+  yield all([fork(watchLoginUser), fork(watchlogout)]);
 }
